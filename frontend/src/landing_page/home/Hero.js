03d7408a473d420aefa1d811dd352d53b2aeeacb
YashpalBhati68 <codercollabnetwork@email.com>
2026-09-06 15:53:19 +0530

    add signup route in home page

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,20 +1,30 @@
 import React from "react";
-
+import { useNavigate } from "react-router-dom";
 function Hero() {
+  const navigate = useNavigate();
   return (
     <div className="container text-center mt-5 pt-5">
       <div className="row">
         <img
           src="/media/images/homeHero.png"
-          alt="image of hero section of home page" 
-          className="mb-5" style={{width:"80%", margin:"0 auto"}}
+          alt="image of hero section of home page"
+          className="mb-5"
+          style={{ width: "80%", margin: "0 auto" }}
         />
         <h2>Invest in everything</h2>
         <p>
           Online platform to invest in stocks, derivatives, mutual funds, ETFs,
           bonds, and more.
         </p>
-        <button className="mt-4 btn btn-primary fs-5" style={{width:"20%", margin:" 0 auto"}} >Sign up for free </button>
+        <button
+          className="mt-4 btn btn-primary fs-5"
+          style={{ width: "20%", margin: " 0 auto" }}
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          Sign up for free{" "}
+        </button>
       </div>
     </div>
   );
